1cdc0b25a1ed56f1f77512e8e0e97c5863d7db0d
flamegit <fxlts@163.com>
2018-10-11 22:21:31 +0800

    add file

---
+++ app/src/main/java/com/example/flame/kotlinstudy/ui/GirlOverViewActivity.kt
@@ -0,0 +1,66 @@
+package com.example.flame.kotlinstudy.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.example.flame.kotlinstudy.R
+import com.example.flame.kotlinstudy.lib.CommonAdapter
+import com.example.flame.kotlinstudy.lib.HtmlPageParser
+import com.example.flame.kotlinstudy.model.Constants
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_girl_overview.*
+
+class GirlOverViewActivity : AppCompatActivity() {
+
+    lateinit var parser: HtmlPageParser
+    var adapter: CommonAdapter<String>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_girl_overview)
+        val url=intent.getStringExtra(Constants.KEY_URL)
+        parser= HtmlPageParser(url)
+        loadFirstImage(url)
+        adapter = CommonAdapter(R.layout.viewholder_girl) { holder, _, data ->
+            Glide.with(this).load(data).into(holder[R.id.image_view])
+        }
+        recycler_view.adapter=adapter
+    }
+
+    private fun loadFirstImage(url: String) {
+        val tmp = Observable.fromCallable {
+            val url = parser.getLadyImage()
+            val count = parser.getLadyNum()
+            return@fromCallable Pair(url, count)
+        }.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    adapter?.addItem(it.first)
+                    loadLeftImage(url, it.second)
+                }
+    }
+
+
+    private fun loadLeftImage(url: String, count: Int) {
+        val dis = Observable.fromIterable(createUrlList(url, count))
+                .map {
+                    HtmlPageParser.getLadyImage(it)
+                }.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    adapter?.addItem(it)
+                }
+    }
+
+    private fun createUrlList(url: String, count: Int): List<String> {
+        var i = 2
+        val list = mutableListOf<String>()
+        while (i <= count) {
+            list.add("$url/$i")
+            i += 1
+        }
+        return list
+    }
+}
